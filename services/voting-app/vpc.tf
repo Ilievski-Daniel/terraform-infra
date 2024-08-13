44e3cccfff646e5afeba6f29a00807785f77aae0
Ilievski-Daniel <ilievskiot@hotmail.com>
2024-08-13 07:54:10 +0200

    Update pipeline, and backend

--- services/voting-app/vpc.tf
+++ services/voting-app/vpc.tf
@@ -1,7 +1,7 @@
 module "voting_app_vpc_network" {
   source = "../../modules/gcp/vpc-network"
 
-  name        = "${local.project_name}-${var.environment}"
+  name        = "${local.project_name}-${var.environment}-a"
   description = var.network_description
   project     = var.gcp_project_id
 }
